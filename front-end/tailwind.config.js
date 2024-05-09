eb6119b6b2177e1eb122d97597829e13cc4560d8
Mohit Goplani <goplanimohit510@gmail.com>
2024-05-09 17:35:07 +0530

    adding the tailwind for theuse of css in the project

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,18 @@
+/*global require*/
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
+}
+
